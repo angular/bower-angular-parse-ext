b14cc6210ceca802d7b36cd05d550865859280ee
jenkins <ci-bot@angularjs.org>
2018-06-12 08:26:41 -0700

    v1.7.2

--- angular-parse-ext.js
+++ angular-parse-ext.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.7.2-build.5558+sha.c9a92fc
+ * @license AngularJS v1.7.2
  * (c) 2010-2018 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -1263,7 +1263,7 @@ angular.module('ngParseExt', [])
   .config(['$parseProvider', function($parseProvider) {
     $parseProvider.setIdentifierFns(isValidIdentifierStart, isValidIdentifierContinue);
   }])
-  .info({ angularVersion: '1.7.2-build.5558+sha.c9a92fc' });
+  .info({ angularVersion: '1.7.2' });
 
 
 })(window, window.angular);
