025e3948ad2747f79b4b6afd3813762507fef9f5
Pete Bacon Darwin <pete@bacondarwin.com>
2018-09-07 10:29:11 +0100

    v1.7.4

--- angular-parse-ext.js
+++ angular-parse-ext.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.7.3
+ * @license AngularJS v1.7.4
  * (c) 2010-2018 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -1263,7 +1263,7 @@ angular.module('ngParseExt', [])
   .config(['$parseProvider', function($parseProvider) {
     $parseProvider.setIdentifierFns(isValidIdentifierStart, isValidIdentifierContinue);
   }])
-  .info({ angularVersion: '1.7.3' });
+  .info({ angularVersion: '1.7.4' });
 
 
 })(window, window.angular);
